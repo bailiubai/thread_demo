4ec11fff180e9dcaf8a5f384117b0c5676d60ea1
liubai <18326395619@163.com>
2021-03-07 17:16:20 +0800

    线程学习

---
+++ src/com/bai/chap1/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bai.chap1;
+
+/**
+ * author:留白
+ * create_by:2021/3/7 16:59
+ * desc:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
